87bf110d6753a2092e6e91fd1d02b45f236a7d81
ouadjih <47463757+ouadjih@users.noreply.github.com>
2022-11-19 15:22:38 +0100

    Added onClick add point feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {React,useState} from 'react'
 
 function App() {
+  const [points,setPoints] = useState([])
+  const [popup,setPopup] = useState([])
+
+  function handleCLick(e){
+   
+    console.log()
+    setPoints([...points,{
+      x:e.clientX,
+      y:e.clientY
+    }]
+    
+    );
+  }
+  function HandleDelete(e){
+    setPopup(points.pop())
+  }
   return (
-    <div className="App">
+    <>
+    <button className='delete' onClick={HandleDelete}>Delete</button>
+    
+    <div className="App" onClick={handleCLick}>
+      {points.map((point)=>
+      
+      <div className='point' style={{position:'absolute',left:point.x-10,top:point.y-20}}>O</div>
+      )
+
+      }
+      
     </div>
+    </>
   );
+
 }
 
 export default App;
